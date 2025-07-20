69b81cce3a36d1f39bb3b7df4916136c5faf4712
Gavin LaMontagne <149981996+Vindude-98@users.noreply.github.com>
2025-07-19 21:03:25 -0600

    Add files via upload

---
+++ Person.cpp
@@ -0,0 +1,21 @@
+/*
+ * FictionPerson.cpp
+ *
+ *  Created on: Jul 19, 2025
+ *      Author: Gavin LaMontagne
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Smith, John" << endl;
+	cout << "123 Cherry Ln" << endl;
+	cout << "Star" << endl;
+	cout << "Idaho" << endl;
+	cout << "99999" << endl;
+
+	return 0;
+}
